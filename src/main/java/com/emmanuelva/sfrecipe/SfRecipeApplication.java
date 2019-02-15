0151d6fe2d1b50f1d2aa0f8cd41dd695b3efc964
falc0mx <1337fal@gmail.com>
2019-02-15 13:57:41 -0600

    Initial commit

---
+++ src/main/java/com/emmanuelva/sfrecipe/SfRecipeApplication.java
@@ -0,0 +1,14 @@
+package com.emmanuelva.sfrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfRecipeApplication.class, args);
+    }
+
+}
+
