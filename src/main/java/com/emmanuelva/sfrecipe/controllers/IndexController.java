4b9c25186baa383758c622f1b87ca3eea42753bf
falc0mx <1337fal@gmail.com>
2019-04-09 20:17:09 -0500

    Models and relations creation

---
+++ src/main/java/com/emmanuelva/sfrecipe/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.emmanuelva.sfrecipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        System.out.println("ajalas morros");
+        return "index";
+    }
+}
